1892f7025aeaf6617137ec0ac92346d2f96611c1
omar hesham <you@example.com>
2022-10-02 01:31:19 +0200

    update lesson_3 unit_3

--- Unit_3/Lesson_3/with_startup.s/startup.s
+++ Unit_3/Lesson_3/with_startup.s/startup.s
@@ -1,33 +1,42 @@
 /*
-	startup_cortexM3.s
-	Eng.Omar Hesham
+startup.s lab3 ###cortex m3###
+Eng.Omar Hesham
 */
 
-
-/* SRAM: 0x20000000 */
+/*SRAM starts from 0x20000000*/
 
 .section .vectors
-.word 0x20001000	// stack address
-.word _reset        // reset address
-.word vector_handler // NMI
-.word vector_handler // hard_fault_handler
-.word vector_handler // MemManage
-.word vector_handler // BusFault
-.word vector_handler // Usage Fault
-.word vector_handler // Reserved
-.word vector_handler // Reserved
-.word vector_handler // Reserved
-.word vector_handler // Reserved
-.word vector_handler // SV Call
-.word vector_handler //
-.word vector_handler //
+
+.word 0x20001000       /* start the stack after this address there is vector table*/
+.word _reset           /* reset */
+.word _vector_handler  /* NMI interrupt */
+.word _vector_handler  /* HARD FAULT interrupt */
+.word _vector_handler  /* MM FAULT interrupt */
+.word _vector_handler  /* BUS FAULT interrupt */
+.word _vector_handler  /* USAGE FAULT interrupt */
+.word _vector_handler  /* RESERVED */
+.word _vector_handler  /* RESERVED */
+.word _vector_handler  /* RESERVED */
+.word _vector_handler  /* RESERVED */
+.word _vector_handler  /* SV CALL */
+.word _vector_handler  /* DEBUG RESERVED */
+.word _vector_handler  /* RESERVED */
+.word _vector_handler  /* PendSV */
+.word _vector_handler  /* SysTick */
+.word _vector_handler  /* IRQ0 */
+.word _vector_handler  /* IRQ1 */
+.word _vector_handler  /* IRQ2 */
+.word _vector_handler  /* ... */
+
+
 
 .section .text
-_reset:
-	bl main
-	b .
+
+_reset: /* underscore 3shan mayb2ash fyh haga shabah el tany fa y7sal conflict */
+ bl main
+ b .
 
 .thumb_func
-vector_handler:
-	b _reset
 
+_vector_handler:
+	b _reset
