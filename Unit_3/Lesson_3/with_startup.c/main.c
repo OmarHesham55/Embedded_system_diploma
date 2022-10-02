a398db56d92dbdf630c5381fa8c5e5019daec393
omar hesham <you@example.com>
2022-10-02 03:56:11 +0200

    update lesson_3 unit_3

---
+++ Unit_3/Lesson_3/with_startup.c/main.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+
+typedef volatile unsigned int vuint32_t;
+#define RCC_BASE 0x40021000
+#define GPIOA_BASE 0x40010800
+
+#define RCC_APB2ENR  *(volatile uint32_t *) (RCC_BASE + 0x18)
+#define GPIO_CRH  *(volatile uint32_t *) (GPIOA_BASE + 0x04)
+#define GPIO_ODR  *(volatile uint32_t *) (RCC_BASE + 0x0C)
+//bit fields
+#define RCC_IOPAEN (1<<2)
+#define GPIOA13    (1UL<<13)
+
+extern void H_fault_handler(void){
+
+}
+
+
+extern void Bus_fault_handler(void){
+
+}
+
+typedef union {
+		vuint32_t  all_fields;
+	struct {
+		vuint32_t  reserved:13 ;
+		vuint32_t  P_13:1 ;
+	} Pin;
+} R_ODR_t;
+
+volatile R_ODR_t* R_ODR = (volatile R_ODR_t*)(GPIOA_BASE + 0x0C);
+
+int main(void)
+{
+
+	RCC_APB2ENR |= RCC_IOPAEN;
+	GPIO_CRH &= 0xFF0FFFFF;
+	GPIO_CRH |= 0x00200000;
+
+	while(1){
+		R_ODR->Pin.P_13 = 1;
+		for(int i=0;i<5000;i++);
+		R_ODR->Pin.P_13 = 0;
+		for(int i=0;i<5000;i++);
+
+	}
+
+	return 0;
+}
