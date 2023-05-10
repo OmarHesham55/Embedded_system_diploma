d8e365f79c3c97c1cc8e34f6769f6676d2113304
omar hesham <you@example.com>
2023-05-10 17:14:35 +0300

    updated

--- Unit_6/Lec4/lab1/Src/main.c
+++ Unit_6/Lec4/lab1/Src/main.c
@@ -6,7 +6,7 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+ * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
  * All rights reserved.</center></h2>
  *
  * This software component is licensed by ST under BSD 3-Clause license,
@@ -17,76 +17,62 @@
  ******************************************************************************
  */
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
-
 #include<stdio.h>
-#include<stdlib.h>
 #include<stdint.h>
+#include<stdlib.h>
 
-//define the Bases
-#define RCC_BASE   	0x40021000
-#define GPIOA_BASE 	0x40010800
-#define INT_BASE	0x40010400
-#define AFIO_BASE   0x40010000
+//BASE_ADDRESS
+#define RCC_BASE	0x40021000
+#define GPIO_BASE	0x40010800
+#define AFIO_BASE	0x40010000
+#define EXTI_BASE	0x40010400
 
+//RCC
+#define RCC_APB2ENR *(volatile uint32_t*)(RCC_BASE + 0x18)
 
-// initialize the GPIO
-#define GPIOA_ODR   *(volatile uint32_t *)(GPIOA_BASE + 0x0C)
-#define GPIOA_CRH   *(volatile uint32_t *)(GPIOA_BASE + 0x04)
-#define GPIOA_CRL	*(volatile uint32_t *)(GPIOA_BASE + 0x00)
-//initialize the AFIO
-#define AFIO_EXTICR1 	*(volatile uint32_t *)(AFIO_BASE + 0x08)
-#define NVIC_EXTI		*(volatile uint32_t *)(0xE000E100)
-//initialize the Clock
-#define RCC_APB    	*(volatile uint32_t *)(RCC_BASE + 0x18)
+//GPIO
+#define GPIOx_CRL	*(volatile uint32_t*)(GPIO_BASE + 0x00)
+#define GPIOx_CRH	*(volatile uint32_t*)(GPIO_BASE + 0x04)
+#define GIOx_ODR	*(volatile uint32_t*)(GPIO_BASE + 0x0C)
+//AFIO
+#define AFIO_EXTICR1 *(volatile uint32_t*)(AFIO_BASE + 0x08)
 
-//initialize the External INT
-#define EXTI_IMR 	*(volatile uint32_t *)(INT_BASE + 0x00)
-#define EXTI_RTSR	*(volatile uint32_t *)(INT_BASE + 0x08)
-#define EXTI_PR 	*(volatile uint32_t *)(INT_BASE + 0x14)
-
-
-
-
-void clock_init(void){
-	//enable clock to AFIO register
-	RCC_APB  |= 1<<0;
-	//enable clock to I/O Port A
-	RCC_APB  |= 1<<2;
+//EXTI
+#define EXTI_IMR	 	*(volatile uint32_t*)(EXTI_BASE + 0x00)
+#define EXTI_RTSR	 	*(volatile uint32_t*)(EXTI_BASE + 0x08)
+#define EXTI_PR		 	*(volatile uint32_t*)(EXTI_BASE + 0x14)
+#define NVIC_ISR0		*(volatile uint32_t*)(0xE000E100)
+void clock_enable(void){
+	RCC_APB2ENR |= (1<<0) | (1<<2);
 }
 
-void GPIO_init(void){
-	GPIOA_CRH &= 0xFF0FFFFF;
-	GPIOA_CRH |= 0x00200000;
-	GPIOA_CRL |= 1<<2;
+void GPIO_ENB(void){
+	//Enable GPIO PinA0 as input
+	GPIOx_CRL |= (0x4<<0);
+
+	//Enable GPIO PinA13 as output
+	GPIOx_CRH &= 0xFF0FFFFF;
+	GPIOx_CRH |= 0x00200000;
 }
 
-void AFIO_init(void){
-	AFIO_EXTICR1 |= 0;  // enable EXTI0
-}
 
 
 int main(void)
 {
-	clock_init();
+	clock_enable();
+	GPIO_ENB();
 
-	GPIO_init();
+	AFIO_EXTICR1 |= 0x0;  	//These bits are written by software to select the source input for EXTIx external interrupt.
 
-	AFIO_init();
+	EXTI_IMR |= (1<<0);		//Interrupt Mask on line 0
+	EXTI_RTSR |= (1<<0);	// Rising trigger event configuration bit of line 0
 
-	EXTI_IMR  |= (1<<0); // enable EXTI line 0 mask1
+	NVIC_ISR0 |= (1<<6);
 
-	EXTI_RTSR |= (1<<0); // enable rising trigger
-
-	NVIC_EXTI |= 1<<6;
 	while(1);
-
 }
 
-
 void EXTI0_IRQHandler(void){
-	GPIOA_ODR ^= 1<<13;
+	GIOx_ODR ^= (1<<13);
 	EXTI_PR |= (1<<0);
 }
