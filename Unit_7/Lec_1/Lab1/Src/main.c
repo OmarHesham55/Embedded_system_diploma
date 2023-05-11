7e5ed51e792adeb8efe59b37c6cc0f61504494ff
omar hesham <you@example.com>
2023-05-11 21:55:49 +0300

    Add Lab1 in Lecture1 unit_7

--- Unit_7/Lec_1/Lab1/Src/main.c
+++ Unit_7/Lec_1/Lab1/Src/main.c
@@ -26,53 +26,78 @@
 #define GPIOA_BASE	0x40010800
 #define AFIO_BASE	0x40010000
 #define EXTI_BASE	0x40010400
-
+#define GPIOB_BASE	0x40010C00
 //RCC
 #define RCC_APB2ENR *(volatile uint32_t*)(RCC_BASE + 0x18)
 
-//GPIO
+//GPIOA
 #define GPIOA_CRL	*(volatile uint32_t*)(GPIOA_BASE + 0x00)
 #define GPIOA_CRH	*(volatile uint32_t*)(GPIOA_BASE + 0x04)
+#define GPIOA_IDR	*(volatile uint32_t*)(GPIOA_BASE + 0x08)
 #define GPIOA_ODR	*(volatile uint32_t*)(GPIOA_BASE + 0x0C)
-//AFIO
-#define AFIO_EXTICR1 *(volatile uint32_t*)(AFIO_BASE + 0x08)
+#define GPIOA_BSRR  *(volatile uint32_t*)(GPIOA_BASE + 0x10)
+
+//GPIOB
+#define GPIOB_CRL	*(volatile uint32_t*)(GPIOB_BASE + 0x00)
+#define GPIOB_CRH	*(volatile uint32_t*)(GPIOB_BASE + 0x04)
+#define GPIOB_IDR	*(volatile uint32_t*)(GPIOB_BASE + 0x08)
+#define GPIOB_ODR	*(volatile uint32_t*)(GPIOB_BASE + 0x0C)
+#define GPIOB_BSRR  *(volatile uint32_t*)(GPIOB_BASE + 0x10)
+
 
 //EXTI
 #define EXTI_IMR	 	*(volatile uint32_t*)(EXTI_BASE + 0x00)
 #define EXTI_RTSR	 	*(volatile uint32_t*)(EXTI_BASE + 0x08)
 #define EXTI_PR		 	*(volatile uint32_t*)(EXTI_BASE + 0x14)
 #define NVIC_ISR0		*(volatile uint32_t*)(0xE000E100)
+
 void clock_enable(void){
-	RCC_APB2ENR |= (1<<0) | (1<<2);
+	//IOPA & IOPB are enabled
+	RCC_APB2ENR |= (1<<3) | (1<<2);
 }
 
 void GPIO_ENB(void){
-	//Enable GPIO PinA0 as input
-	GPIOA_CRL |= (0x4<<0);
+	// Configure PinA 1,13 as input
+//PinA1
+	GPIOA_CRL &=~(0b11<<4);
+	GPIOA_CRL |= (1<<6);
+	GPIOA_CRL &=~(1<<4);
+//PinA13
+	GPIOA_CRL &=~(0b11<<20);
+	GPIOA_CRL |= (1<<22);
+	GPIOA_CRL &=~(1<<23);
 
-	//Enable GPIO PinA13 as output
-	GPIOA_CRH &= 0xFF0FFFFF;
-	GPIOA_CRH |= 0x00200000;
+	// Configure PinB 1,13 as output
+//PinB1
+	GPIOB_CRL |= (1<<4);
+	GPIOB_CRL &=~(0b111<<5);
+//PinB13
+	GPIOB_CRH |= (1<<20);
+	GPIOB_CRH &=~(0b111<<21);
+
 }
 
-
+void wait(int x){
+	for(int i=0; i<x;i++){
+		for(int j=0; j<500;j++);
+	}
+}
 
 int main(void)
 {
 	clock_enable();
 	GPIO_ENB();
+	while(1){
+			if( ( (GPIOA_IDR>>1) && 1 ) == 0 ){
+				GPIOB_ODR ^= (1<<1);
+				while(( (GPIOA_IDR>>1) && 1 ) == 0);
+			}
+			if( ( (GPIOA_IDR>>13) && 1 ) == 1 ){
+				GPIOB_ODR ^= (1<<13);
+			}
 
-	AFIO_EXTICR1 |= 0x0;  	//These bits are written by software to select the source input for EXTIx external interrupt.
-
-	EXTI_IMR |= (1<<0);		//Interrupt Mask on line 0
-	EXTI_RTSR |= (1<<0);	// Rising trigger event configuration bit of line 0
-
-	NVIC_ISR0 |= (1<<6);
-
-	while(1);
+			wait(1);
+	}
 }
 
-void EXTI0_IRQHandler(void){
-	GPIOA_ODR ^= (1<<13);
-	EXTI_PR |= (1<<0);
-}
+
